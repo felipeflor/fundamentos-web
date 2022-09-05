d8f382f6292f2d53e92fdf73f3d388d2ed3984b9
Felipe <felipedossantosflor@gmail.com>
2022-09-05 11:22:42 -0300

    @FelipeFlor [Task: JS]

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let assunto = document.querySelector("#assunto")
 let nomeOk = false
 let emailOk = false
 let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 
 //nome.style.width = "100%"
@@ -53,4 +54,14 @@ function enviar() {
     } else {
         alert ("Preencha todos os campos corretamente")
     }
+}
+
+function mapaZoom () {
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+
+function mapaZoom () {
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
 }
